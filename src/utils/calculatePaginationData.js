107743e22f653d5080981ff71506cb4850f1e6f2
Oleh Shalashov <spirit14081992@gmail.com>
2025-05-13 12:14:49 -0300

    Update minor fix

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,14 +1,10 @@
 export const calculatePaginationData = (count, perPage, page) => {
 	const totalPages = Math.ceil(count / perPage);
-	const hasNextPage = page < totalPages;
-	const hasPreviousPage = page > 1;
 
 	return {
 		page,
 		perPage,
 		totalItems: count,
 		totalPages,
-		hasPreviousPage,
-		hasNextPage,
 	};
 };
